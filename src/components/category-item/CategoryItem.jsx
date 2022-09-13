62333d995064fe61767a9793f5b46a7556fbe4ea
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-13 14:42:52 +0530

    created directory and category cards

---
+++ src/components/category-item/CategoryItem.jsx
@@ -0,0 +1,20 @@
+//CSS
+import './CategoryItem.scss'
+
+function CategoryItem({ category }) {
+    return (
+        <div className="category-container" key={category.id}>
+            <div 
+                className="background-image" 
+                style={{backgroundImage: `url(${category.imageUrl})`}}
+            >
+            </div>
+            <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
