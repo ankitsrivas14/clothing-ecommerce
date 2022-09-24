1c525590360ed4958796a5169c66499e7454dfc6
Ankit Srivastava <ankitsrivas14@gmail.com>
2022-09-24 23:08:40 +0530

    completed userReducer

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,5 +1,5 @@
 //REACT
-import { createContext, useState, useEffect } from "react"
+import { createContext, useState, useEffect, useReducer } from "react"
 
 //FIREBASE
 import { onAuthStateChangedListener, createUserDocumentFromAuth, signOutUser } from "../utils/firebase/firebase";
@@ -9,9 +9,36 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type){
+        case 'SET_CURRENT_USER':
+            return {
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type: ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser] = useState(null);
+    const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    const { currentUser } = state;
+
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user});
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListener((user) => {
             if(user){
